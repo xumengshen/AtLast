c1a29eb3662ab2a74050ade5a840235917f70a9c
pc <584010041@qq.com>
2018-01-26 14:53:58 +0800

    徐蒙伸做了一次修改

--- app/src/main/java/com/three/pc/atlast/MainActivity.java
+++ app/src/main/java/com/three/pc/atlast/MainActivity.java
@@ -9,10 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        xmsText();
-    }
-
-    public void xmsText(){
 
     }
+
+
 }
